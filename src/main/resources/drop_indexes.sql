559da000c4b108fd84b6ee8b234acadd855ffc67
Pavel_Bortnik <pavel_bortnik@epam.com>
2021-03-16 11:58:59 +0300

    remove hardcoded database name

--- src/main/resources/drop_indexes.sql
+++ src/main/resources/drop_indexes.sql
@@ -45,7 +45,7 @@ ALTER TABLE issue
     DROP CONSTRAINT IF EXISTS issue_issue_id_fkey;
 DROP INDEX IF EXISTS issue_it_idx;
 DROP INDEX IF EXISTS ticket_submitter_idx;
-ALTER TABLE reportportal.public.issue_ticket
+ALTER TABLE public.issue_ticket
     DROP CONSTRAINT IF EXISTS issue_ticket_issue_id_fkey,
     DROP CONSTRAINT IF EXISTS issue_ticket_ticket_id_fkey;
 
